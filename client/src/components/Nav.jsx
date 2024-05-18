98d96fc629117ca3f33429a3e68ee2a2a8473c39
bikram@dey2003 <62688713+modhack2003@users.noreply.github.com>
2024-05-18 09:23:42 +0530

    hello

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,13 +1,30 @@
 // Navigation.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoCloseSharp } from "react-icons/io5";
 import './Nav.css'
 
 const Nav = () => {
+  const showSidebar = ()=> {
+    const sidebar = document.querySelector('.sidebar');
+    
+    sidebar.style.display = 'flex'
+    
+  }
+  const hideSidebar = ()=> {
+    const sidebar = document.querySelector('.sidebar');
+   
+    sidebar.style.display = 'none'
+
+  }
+
   return (
     <nav>
-      <ul>
-        <li >
+      <ul className='sidebar'>
+      <li onClick={hideSidebar} className='close'><IoCloseSharp  className='close-icon'/>
+      </li>
+      <li >
           <Link to="/" className='a' >Home</Link>
         </li>
         <li>
@@ -19,6 +36,22 @@ const Nav = () => {
         <li>
           <Link to="/signup"  className='a'>Signup</Link>
         </li>
+        
+      </ul>
+      <ul>
+        <li className='hideOnMobile' >
+          <Link to="/" className='a' >Home</Link>
+        </li>
+        <li className='hideOnMobile'>
+          <Link to="/task"  className='a'>Task</Link>
+        </li>
+        <li className='hideOnMobile'>
+          <Link to="/login"  className='a'>Login</Link>
+        </li>
+        <li className='hideOnMobile'>
+          <Link to="/signup"  className='a'>Signup</Link>
+        </li>
+        <li className='menu' onClick={showSidebar}><RxHamburgerMenu  className='menu-icon'/></li>
       </ul>
     </nav>
   );
